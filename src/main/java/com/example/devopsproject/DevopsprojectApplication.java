1e4452762adeb0d3124ec362dcd4a5c70051d448
Shrikumar <shri@linuxmail.org>
2020-12-15 09:27:41 +0530

    Initial Commit

---
+++ src/main/java/com/example/devopsproject/DevopsprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.devopsproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsprojectApplication.class, args);
+	}
+
+}
